9b0f92654bc6c7e12ccc939fa33945f9a1f4a48d
soficede <sofia.cedeno@udea.edu.co>
2024-08-20 19:40:47 -0500

    1

---
+++ Ejercicio1pacientes.py
@@ -0,0 +1,10 @@
+class Paciente:
+    def __init__(self):
+        self.__nombre = ""
+        self.__cedula = 0
+        self.__genero = ""
+        self.__servicio = ""
+
+    def verNombre(self):
+        return self.__nombre
+    
